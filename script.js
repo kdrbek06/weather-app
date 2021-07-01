64f774843117265bc7f6fd12939ac83b2fac604a
kdrbek06 <kdrbek@gmail.com>
2021-07-01 13:10:40 +0200

    API code integrated and current temperature added to index html

--- script.js
+++ script.js
@@ -4,15 +4,31 @@ const apiKey = "d865f40a995be1311f0b106a8c307587"
 
 
 const updateData =  function(data){
-    console.log(console.log(data));
-    // inner html ile kutunun icerigini doldur
+    console.log(data);
 
-    document.getElementById('currentBox').innerHTML = data.list[0].main.temp;
+
+    
+    document.getElementById('currentImg').src = `http://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`;
+    document.getElementById('cityName').innerHTML = data.city.name
+    document.getElementById('weatherDescription').innerHTML = data.list[0].weather[0].description;
+    document.getElementById('temp').innerHTML = `Now:   ${Math.floor(data.list[0].main.temp)}  °C`;
+    document.getElementById('tempFeel').innerHTML = `Feels Like:   ${Math.floor(data.list[0].main.feels_like)}  °C`;
+    document.getElementById('tempMin').innerHTML = `Min:   ${Math.floor(data.list[0].main.temp)}  °C`;
+    document.getElementById('tempMax').innerHTML = `Max:   ${Math.floor(data.list[0].main.temp)}  °C`;
+    document.getElementById('humidity').innerHTML = `Humidity:  % ${Math.floor(data.list[0].main.humidity)}  `;
+
+    /* let img = document.getElementById('currentImg'); */
+    // img.setAttribute('src',  `"http://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png"`);
+
 };
 
 document.getElementById('searchButton').addEventListener('click', () =>{
-    let userCity= document.getElementById('inputBox').value
-    axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=${userCity}&appid=${apiKey}&units=metric`)
+    let inputCity= document.getElementById('inputBox').value
+    axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=${inputCity}&appid=${apiKey}&units=metric`)
     .then(res => updateData(res.data))
     .catch(err=> console.log(err));   
 })
+
+
+
+// http://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png
